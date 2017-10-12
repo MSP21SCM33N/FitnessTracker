7b383a33db816c73bccbf3d24af4428d7da4faa1
ajhosek <32681415+ajhosek@users.noreply.github.com>
2017-10-12 11:40:54 -0500

    Add files via upload

---
+++ Fitness Project.py
@@ -0,0 +1,13 @@
+
+
+miles = int(input("Welcome to the calorie calculator!\nHow far did you travel today? (Miles)"))
+weight = int(input("How much do you weigh?(lbs)"))
+travel = input("Finally, did you run or walk?")
+
+if travel == 'walk':
+    caloriesBurnt = .53 *weight * miles
+    print("You burnt ", caloriesBurnt, " calories while walking today!")
+elif travel == 'run':
+    caloriesBurnt = .75 * weight * miles
+    print("You burnt ", caloriesBurnt, " calories while running today!")
+
